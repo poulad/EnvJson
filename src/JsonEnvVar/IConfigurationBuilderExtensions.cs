469c7c4f1f8ba672da6d6a7d6b8f241c57c21e1c
Poulad <poulad.pld@gmail.com>
2018-10-20 09:51:04 -0400

    Upgrade to v1

--- src/JsonEnvVar/IConfigurationBuilderExtensions.cs
+++ src/JsonEnvVar/IConfigurationBuilderExtensions.cs
@@ -2,8 +2,18 @@
 
 namespace Microsoft.Extensions.Configuration
 {
+    /// <summary>
+    /// Contains extension methods for adding JSON-serialized environment variables
+    /// </summary>
     public static class ConfigurationBuilderExtensions
     {
+        /// <summary>
+        /// Adds JSON-serialized environment variable to the configuration builder
+        /// </summary>
+        /// <param name="configurationBuilder">The instance of <see cref="IConfigurationBuilder"/> to add to</param>
+        /// <param name="envVar">Environment variable name</param>
+        /// <param name="optional">Whether the environment variable is optional</param>
+        /// <returns>The instance of <see cref="IConfigurationBuilder"/></returns>
         public static IConfigurationBuilder AddJsonEnvVar(
             this IConfigurationBuilder configurationBuilder,
             string envVar,
